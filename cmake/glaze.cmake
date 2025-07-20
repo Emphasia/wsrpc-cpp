b94c0fe08b4b58245a9d729cc84844c2f791e3ef
coco <574402766@qq.com>
2025-07-20 18:00:00 +0800

    build: glaze

--- cmake/glaze.cmake
+++ cmake/glaze.cmake
@@ -8,6 +8,7 @@ CPMAddPackage(
   URL https://github.com/stephenberry/glaze/archive/refs/tags/v5.5.0.zip
   SYSTEM YES
   OPTIONS
+    "glaze_INCLUDES_WITH_SYSTEM OFF"
     "glaze_BUILD_TESTS OFF"
     "glaze_ENABLE_AVX2 ON"
 )
